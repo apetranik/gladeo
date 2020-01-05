24b525b1cb45a1750a67cba05d63312e80b3f92d
Aliya Petranik <petranik@usc.edu>
2020-01-04 15:58:44 -1000

    coverage articfacts

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['./__tests__/setup.js'],
+  reporters: ["default", "jest-junit"],
 }
